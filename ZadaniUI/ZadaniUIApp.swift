0bd5f3ae76960aa88ff8da561aa72f0a3571b26b
Firdavs <firdavsvlog@gmail.com>
2021-10-29 10:23:18 +0500

    Initial Commit

---
+++ ZadaniUI/ZadaniUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZadaniUIApp.swift
+//  ZadaniUI
+//
+//  Created by User on 29/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ZadaniUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
